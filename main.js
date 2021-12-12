c5f5ba49590056796bbd99ce2e9526d5cbb42edd
Katherine Temkin <k@ktemkin.com>
2021-12-12 00:32:53 -0700

    css tweaks for mobile toolbar'ing

--- main.js
+++ main.js
@@ -429,7 +429,7 @@ const jsrepl = {};
     if (this.toolbar && !app.isMobile) {
       paddingBottom = "32px";
     } else if (app.isMobile) {
-      paddingBottom = "84pt";
+      paddingBottom = "60pt";
     }
 
     // HyperScript Notation
